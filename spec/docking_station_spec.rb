26934e6b35d90057a984b5b4acdb8bd9bfe1acfe
Maria Vanaale <mvanaale@gmail.com>
2019-02-07 14:16:49 +0000

    step13

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -36,5 +36,13 @@ describe DockingStation do
     end
   end
 
+  describe '#dock' do
+    bike = Bike.new
+    # subject.dock(bike)
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect{ subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
 end
   #check if the bike is working
