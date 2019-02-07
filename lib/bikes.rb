538b2323f94297079fcce1aa8fe6de4da4caae07
Maria Vanaale <mvanaale@gmail.com>
2019-02-07 15:58:36 +0000

    step 12 complete

---
+++ lib/bikes.rb
@@ -0,0 +1,9 @@
+class Bikes
+ @bikes = []
+#   array =[]
+#   if array.length >= 20
+#     return "Bike stack full"
+#   elsif array == 0
+#     return "empty"
+#   end
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,48 +1,86 @@
-# In spec/borisbike_spec.rb
-require "./lib/docking_station"
+require 'docking_station'
+
 describe DockingStation do
-  #it 'release_bike' do
-  it {is_expected.to respond_to :release_bike}
-
-  it 'releases working bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
-
-  it {is_expected.to respond_to(:dock).with(1).argument }
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
-
-  it { is_expected.to respond_to(:bike) }
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
+  it { is_expected.to respond_to(:release_bike)}
 
   describe '#release_bike' do
-
-    it 'releases a bike' do
+    it 'releases working bike' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
-    it 'raises an error when there are no bikes available' do
-      expect { subject.release_bike }.to raise_error 'No bikes available'
+    it 'raises an error when no bikes available' do
+      expect{ subject.release_bike}.to raise_error 'No bikes available'
     end
   end
 
   describe '#dock' do
-    bike = Bike.new
-    # subject.dock(bike)
-    it 'raises an error when full' do
-      subject.dock(Bike.new)
-      expect{ subject.dock Bike.new }.to raise_error 'Docking station full'
+    it 'returns an error when full' do
+      20.times { subject.dock Bike.new}
+      expect{subject.dock Bike.new}.to raise_error 'Dock station full'
     end
   end
+
+
+  it 'docks something' do
+    bike = Bike.new
+    array = []
+    array.push(bike)
+    expect(subject.dock (bike)).to eq array
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+  end
 end
-  #check if the bike is working
+
+# # In spec/borisbike_spec.rb
+# require "./lib/docking_station"
+# describe DockingStation do
+#
+#   it {is_expected.to respond_to :release_bike}
+#  describe
+#   it 'releases working bikes' do
+#     bike = Bike.new
+#     subject.dock(bike)
+#     expect(subject.release_bike).to eq bike
+#   end
+#
+#   it {is_expected.to respond_to(:dock).with(1).argument }
+#   it 'docks something' do
+#     bike = Bike.new
+#     array = []
+#     array.push(bike)
+#     expect(subject.dock(bike.class)).to eq bike
+#   end
+#
+#   it { is_expected.to respond_to(:bike) }
+#   it 'returns docked bikes' do
+#     bike = Bike.new
+#     subject.dock(bike)
+#     expect(subject.bike).to eq bike
+#   end
+#
+#   describe '#release_bike' do
+#
+#     it 'releases a bike' do
+#       bike = Bike.new
+#       subject.dock(bike)
+#       expect(subject.release_bike).to eq bike
+#     end
+#     it 'raises an error when there are no bikes available' do
+#       expect { subject.release_bike }.to raise_error 'No bikes available'
+#     end
+#   end
+#
+#   describe '#dock' do
+#     # subject.dock(bike)
+#     it 'raises an error when full' do
+#       20.times { subject.dock Bike.new }
+#       expect{ subject.dock Bike.new }.to raise_error 'Docking station full'
+#     end
+#   end
+# end
+#   #check if the bike is working
