7ebb3bf12f27d4e67fd1c80ee791f857ee002729
Maria Vanaale <mvanaale@gmail.com>
2019-02-07 09:38:01 +0000

    complete step 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,8 @@ describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 
   it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
     bike = subject.release_bike
     expect(bike).to be_working
   end
@@ -23,6 +25,7 @@ describe DockingStation do
   end
 
   describe '#release_bike' do
+
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
